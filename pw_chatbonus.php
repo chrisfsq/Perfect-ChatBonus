24b7f14f2ae1b6a8c60ec1c8793b05f58a7d2c36
chrisfsq <christianfsq@gmail.com>
2024-10-07 14:43:37 -0300

    Update dir's and api instance

--- pw_chatbonus.php
+++ pw_chatbonus.php
@@ -1,25 +1,24 @@
 <?php
 
-require('./config.php');
+require('./configs/config.php');
 require('./msg.php');
-require('./pwapi.php');
+require('./api/PwAPI.php');
+
+$api = new API;
 
 $argv[1]($argv[2]);
-
-
 function sendBonus($line = null)
 {
-    global $config;
-    global $msg;
+    global $config, $msg, $api;
 
     if (@fsockopen('127.0.0.1', $config["ports"]['gamedbd'], $errCode, $errStr, 1)) {
         if (strpos($line, "msg=IQBCAE8ATgBVAFMA") !== false and strpos($line, "chl=0") !== false) {
             $onlineGM = explode("=", explode(":",  $line)[5])[1];
-            $isGM = getRoleBase($onlineGM);
+            $isGM = $api->getRoleBase($onlineGM);
             $userID = $isGM['userid'];
             $goldBonus = $config['goldBonus'] * 100;
 
-            $getStatus = getRoleStatus($onlineGM);
+            $getStatus = $api->getRoleStatus($onlineGM);
             $level = $getStatus['status']['level2']; //cultivation level
 
             $mysqli = new mysqli($config['mysql']['host'], $config['mysql']['user'], $config['mysql']['password'], $config['mysql']['db']);
@@ -51,7 +50,7 @@ function sendBonus($line = null)
                 $key = mt_rand(0, count($msg) - 1);
                 $msg[$key] = str_replace('{{cultivation}}', $cultString, $msg[$key]);
                 $msg[$key] = str_replace('{{bonus}}', $config['goldBonus'], $msg[$key]);
-                chatInGame($msg[$key], $gmId);
+                $api->chatInGame($msg[$key], $gmId);
             
                 $date = date("Y-m-d H:i:s");
                 $sqlInsertCash = "INSERT INTO `usecashnow`(userid, zoneid, sn, aid, point, cash, status, creatime) VALUES ('$userID', '1', '0', '1', '0', '$goldBonus', '1', '$date')";
